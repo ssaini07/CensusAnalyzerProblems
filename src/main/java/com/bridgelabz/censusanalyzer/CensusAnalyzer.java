5168b541a691be68289ae7a32223d8dc0f2ae1eb
ssaini07 <78027352+ssaini07@users.noreply.github.com>
2022-01-04 15:25:43 +0530

    Added: Showing welcome messgage

---
+++ src/main/java/com/bridgelabz/censusanalyzer/CensusAnalyzer.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.censusanalyzer;
+
+public class CensusAnalyzer {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Census Analyzer Problems!!");
+    }
+}
